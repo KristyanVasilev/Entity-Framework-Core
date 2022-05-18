a520a2f556dda96e7f9d362883f2954a6f0c77ab
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-05-18 19:13:30 +0300

    Ado.Net

---
+++ ADO.NET/Introduction/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=DESKTOP-KQDB4SL\\SQLEXPRESS;DataBase=SoftUni;Trusted_Connection=True;Encrypt=false";
+            var connection = new SqlConnection(connectionString);
+            {
+                connection.Open();
+                var query = "UPDATE Employees SET Salary = Salary + 10";
+                SqlCommand sqlCommand = new SqlCommand(query, connection);
+
+                var rowsAffected = sqlCommand.ExecuteNonQuery();
+                Console.WriteLine(rowsAffected);
+            }
+        }
+    }
+}

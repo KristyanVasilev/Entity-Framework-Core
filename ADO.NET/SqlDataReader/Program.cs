dd3293d286e72fa990cebcaa1b17ec526983bef5
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-05-19 22:25:36 +0300

    SqlDataReader

---
+++ ADO.NET/SqlDataReader/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace SqlDataReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=DESKTOP-KQDB4SL\\SQLEXPRESS;DataBase=SoftUni;Trusted_Connection=True;Encrypt=false";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var query = "SELECT * FROM Employees";
+                SqlCommand sqlCommand = new SqlCommand(query, connection);
+                var sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Console.WriteLine($"{sqlDataReader["FirstName"]} {sqlDataReader["LastName"]}" +
+                        $" have annual salary {sqlDataReader["Salary"]:f2}$");
+                }
+
+                sqlDataReader.Close();
+            }
+        }
+    }
+}

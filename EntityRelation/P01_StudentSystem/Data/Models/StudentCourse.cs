44d45a434a5116e38a772735384abe83dbb47119
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-06-01 09:47:56 +0300

    Relation with Fluent Api

---
+++ EntityRelation/P01_StudentSystem/Data/Models/StudentCourse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class StudentCourse //Relation with mapping class -> ef 3.1 or 5.0 with Fluent Api
+    {
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+        
+    }
+}
---
+++ EntityRelation/P01_StudentSystem/Data/Models/StudentSystemContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>().HasKey(x => new
+            {
+                x.StudentId,
+                x.CourseId
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Integrated Security=true;Database=StudentSystem");
+            }
+        }
+    }
+}

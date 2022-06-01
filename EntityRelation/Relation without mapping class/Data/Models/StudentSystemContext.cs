bf92ce0fea51ceb096dfb8f94fe2e5f1fabf7d28
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-06-01 10:34:39 +0300

    Initial commit

---
+++ EntityRelation/Relation without mapping class/Data/Models/StudentSystemContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Integrated Security=true;Database=StudentSystem");
+            }
+        }
+    }
+}
---
+++ EntityRelation/Relation without mapping class/Program.cs
@@ -0,0 +1,18 @@
+﻿using P01_StudentSystem.Data.Models;
+using System;
+
+namespace P01_StudentSystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var db = new StudentSystemContext();
+
+            //better use migrations
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+        }
+    }
+}

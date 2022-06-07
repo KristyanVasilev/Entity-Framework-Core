0802a186b382f2f0ca787d531648f7c7d8932f42
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-06-07 14:49:45 +0300

    C# Auto Mapping Objects

---
+++ C# Auto Mapping Objects/Solution/FastFood.Core/ViewModels/Positions/CreatePositionInputModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Core.ViewModels.Positions
+{
+    public class CreatePositionInputModel
+    {
+        [Required]
+        public string PositionName { get; set; }
+    }
+}
